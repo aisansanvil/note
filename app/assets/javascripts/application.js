5c8d0fe55ca5001b418aa66f7b77e45bb611a6d3
Miho Umezawa <m.umecco@gmail.com>
2017-10-20 08:35:19 +0900

    20171020

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,16 @@
 //= require twitter/bootstrap
 //= require bootstrap
 //= require_tree .
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script>
+$(function () {
+  var $body = $('body');
+  $('#js__sideMenuBtn').on('click', function () {
+    $body.toggleClass('side-open');
+    $('#js__overlay').on('click', function () {
+      $body.removeClass('side-open');
+    });
+  });
+});
+</script>
